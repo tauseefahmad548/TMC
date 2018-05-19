546f12e7558d42de8e03e58c4b13720d90ab334d
Tauseef Ahmad <tauseefahmad548@gmail.com>
2018-05-19 15:39:27 +0500

    init

---
+++ app/src/main/java/com/example/tauseef/tmc/activity/ShiftsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.tauseef.tmc.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tauseef.tmc.R
+
+class ShiftsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shifts)
+    }
+}
