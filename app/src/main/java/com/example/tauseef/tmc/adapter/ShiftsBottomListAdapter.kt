70e865956b885d2668f880699af4b4fec52758a4
Tauseef Ahmad <tauseefahmad548@gmail.com>
2018-05-22 14:42:02 +0500

    init

--- app/src/main/java/com/example/tauseef/tmc/adapter/ShiftsBottomListAdapter.kt
+++ app/src/main/java/com/example/tauseef/tmc/adapter/ShiftsBottomListAdapter.kt
@@ -7,19 +7,18 @@ import android.view.ViewGroup
 import android.widget.TextView
 import com.example.tauseef.tmc.R
 
-class ShiftsBottomListAdapter (): RecyclerView.Adapter<ShiftsBottomListAdapter.VH>() {
+class ShiftsBottomListAdapter (val personneList:List<Array<String>>): RecyclerView.Adapter<ShiftsBottomListAdapter.VH>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShiftsBottomListAdapter.VH {
        val view:View = LayoutInflater.from(parent.context).inflate(R.layout.shifts_bottom_list_item,parent,false)
         val vh:VH = VH(view)
         return  vh
     }
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getItemCount(): Int = personneList.size
 
     override fun onBindViewHolder(holder: ShiftsBottomListAdapter.VH, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.shiftslistItemRankTextView.setText(personneList.get(position)[0])
+        holder.shiftsListItemNameTextView.setText(personneList.get(position)[1])
     }
 
     class VH(val view: View) : RecyclerView.ViewHolder(view) {
