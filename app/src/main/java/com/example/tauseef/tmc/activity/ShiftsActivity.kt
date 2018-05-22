ae260dbdc3698033f82dcf1fa989c406bab25d6d
Tauseef Ahmad <tauseefahmad548@gmail.com>
2018-05-22 21:43:11 +0500

    init

--- app/src/main/java/com/example/tauseef/tmc/activity/ShiftsActivity.kt
+++ app/src/main/java/com/example/tauseef/tmc/activity/ShiftsActivity.kt
@@ -1,5 +1,6 @@
 package com.example.tauseef.tmc.activity
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
@@ -7,18 +8,32 @@ import android.support.v7.widget.RecyclerView
 import com.example.tauseef.tmc.R
 import com.example.tauseef.tmc.adapter.ShiftsBottomListAdapter
 import kotlinx.android.synthetic.main.activity_shifts.*
+import kotlinx.android.synthetic.main.shift_sections.view.*
 import java.util.stream.Collectors.toList
 
 class ShiftsActivity : AppCompatActivity() {
+    val context: Context = this
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shifts)
         val bottomListLayoutManager:RecyclerView.LayoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        val shiftsLayoutManager:RecyclerView.LayoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
         val bottomListAdapter: ShiftsBottomListAdapter = ShiftsBottomListAdapter(getDummyData());
         var bottomListRecyclerView: RecyclerView = findViewById(R.id.bottom_list_recycler_view)
         bottomListRecyclerView.adapter = bottomListAdapter
         bottomListRecyclerView.layoutManager = bottomListLayoutManager
+
+        val morningDutiesList:RecyclerView = include2.shift_section_reycler_ciew
+        val afternoonDutiesList:RecyclerView = include2.shift_section_reycler_ciew
+        val nightDutiesList:RecyclerView = include2.shift_section_reycler_ciew
+        val omLeaveList:RecyclerView = include2.shift_section_reycler_ciew
+        val hospitalDutiesList:RecyclerView = include2.shift_section_reycler_ciew
+        val GCrewDutiesList:RecyclerView = include2.shift_section_reycler_ciew
+        val tempDutiesList:RecyclerView = include2.shift_section_reycler_ciew
+        val securityDutiesList:RecyclerView = include2.shift_section_reycler_ciew
+
+
 
 
 
@@ -32,5 +47,7 @@ class ShiftsActivity : AppCompatActivity() {
         aList.add(arrayOf("Cpl/T", "Sajid"))
         return aList;
     }
+
+
 }
 
