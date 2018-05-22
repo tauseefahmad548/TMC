c7d14428bb352eec7301419371f9ccf9ba68bf80
Tauseef Ahmad <tauseefahmad548@gmail.com>
2018-05-22 22:33:55 +0500

    init

--- app/src/main/java/com/example/tauseef/tmc/activity/ShiftsActivity.kt
+++ app/src/main/java/com/example/tauseef/tmc/activity/ShiftsActivity.kt
@@ -7,34 +7,61 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.example.tauseef.tmc.R
 import com.example.tauseef.tmc.adapter.ShiftsBottomListAdapter
+import com.example.tauseef.tmc.listener.ShiftsDragListener
 import kotlinx.android.synthetic.main.activity_shifts.*
 import kotlinx.android.synthetic.main.shift_sections.view.*
 import java.util.stream.Collectors.toList
+import kotlin.coroutines.experimental.coroutineContext
 
 class ShiftsActivity : AppCompatActivity() {
-    val context: Context = this
+    public val context: Context = this
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shifts)
-        val bottomListLayoutManager:RecyclerView.LayoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
-        val shiftsLayoutManager:RecyclerView.LayoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        val bottomListLayoutManager: RecyclerView.LayoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        val shiftsLayoutManager: RecyclerView.LayoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         val bottomListAdapter: ShiftsBottomListAdapter = ShiftsBottomListAdapter(getDummyData());
         var bottomListRecyclerView: RecyclerView = findViewById(R.id.bottom_list_recycler_view)
+
         bottomListRecyclerView.adapter = bottomListAdapter
         bottomListRecyclerView.layoutManager = bottomListLayoutManager
 
-        val morningDutiesList:RecyclerView = include2.shift_section_reycler_ciew
-        val afternoonDutiesList:RecyclerView = include2.shift_section_reycler_ciew
-        val nightDutiesList:RecyclerView = include2.shift_section_reycler_ciew
-        val omLeaveList:RecyclerView = include2.shift_section_reycler_ciew
-        val hospitalDutiesList:RecyclerView = include2.shift_section_reycler_ciew
-        val GCrewDutiesList:RecyclerView = include2.shift_section_reycler_ciew
-        val tempDutiesList:RecyclerView = include2.shift_section_reycler_ciew
-        val securityDutiesList:RecyclerView = include2.shift_section_reycler_ciew
+        val morningDutiesList: RecyclerView = include2.shift_section_reycler_ciew
+        val afternoonDutiesList: RecyclerView = include2.shift_section_reycler_ciew
+        val nightDutiesList: RecyclerView = include2.shift_section_reycler_ciew
+        val omLeaveList: RecyclerView = include2.shift_section_reycler_ciew
+        val hospitalDutiesList: RecyclerView = include2.shift_section_reycler_ciew
+        val GCrewDutiesList: RecyclerView = include2.shift_section_reycler_ciew
+        val tempDutiesList: RecyclerView = include2.shift_section_reycler_ciew
+        val securityDutiesList: RecyclerView = include2.shift_section_reycler_ciew
 
+        morningDutiesList.adapter = bottomListAdapter
+        afternoonDutiesList.adapter = bottomListAdapter
+        nightDutiesList.adapter = bottomListAdapter
+        omLeaveList.adapter = bottomListAdapter
+        hospitalDutiesList.adapter = bottomListAdapter
+        GCrewDutiesList.adapter = bottomListAdapter
+        tempDutiesList.adapter = bottomListAdapter
+        securityDutiesList.adapter = bottomListAdapter
 
+        morningDutiesList.layoutManager = shiftsLayoutManager
+        afternoonDutiesList.layoutManager = shiftsLayoutManager
+        nightDutiesList.layoutManager = shiftsLayoutManager
+        omLeaveList.layoutManager = shiftsLayoutManager
+        hospitalDutiesList.layoutManager = shiftsLayoutManager
+        GCrewDutiesList.layoutManager = shiftsLayoutManager
+        tempDutiesList.layoutManager = shiftsLayoutManager
+        securityDutiesList.layoutManager = shiftsLayoutManager
 
+        morningDutiesList.setOnDragListener(ShiftsDragListener())
+        afternoonDutiesList.setOnDragListener(ShiftsDragListener())
+        nightDutiesList.setOnDragListener(ShiftsDragListener())
+        omLeaveList.setOnDragListener(ShiftsDragListener())
+        hospitalDutiesList.setOnDragListener(ShiftsDragListener())
+        GCrewDutiesList.setOnDragListener(ShiftsDragListener())
+        tempDutiesList.setOnDragListener(ShiftsDragListener())
+        securityDutiesList.setOnDragListener(ShiftsDragListener())
 
 
     }
--- app/src/main/java/com/example/tauseef/tmc/listener/ShiftsDragListener.kt
+++ app/src/main/java/com/example/tauseef/tmc/listener/ShiftsDragListener.kt
@@ -5,8 +5,9 @@ import android.view.View
 import android.view.View.OnDragListener
 
 class ShiftsDragListener :  OnDragListener{
-    override fun onDrag(v: View?, event: DragEvent?): Boolean {
-        when (DragEvent){
+    override fun onDrag(v: View, event: DragEvent): Boolean {
+        val action:Int = event.action
+        when (action){
             DragEvent.ACTION_DRAG_STARTED -> {}
             DragEvent.ACTION_DRAG_ENTERED ->{}
 
