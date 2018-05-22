7646199aa5f46fe2c4125ff1303f4dafff5fa1b8
Tauseef Ahmad <tauseefahmad548@gmail.com>
2018-05-22 14:31:22 +0500

    init

--- app/src/main/java/com/example/tauseef/tmc/adapter/ShiftsBottomListAdapter.kt
+++ app/src/main/java/com/example/tauseef/tmc/adapter/ShiftsBottomListAdapter.kt
@@ -1,4 +1,35 @@
 package com.example.tauseef.tmc.adapter
 
-class ShiftsBottomListAdapter {
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.tauseef.tmc.R
+
+class ShiftsBottomListAdapter (): RecyclerView.Adapter<ShiftsBottomListAdapter.VH>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShiftsBottomListAdapter.VH {
+       val view:View = LayoutInflater.from(parent.context).inflate(R.layout.shifts_bottom_list_item,parent,false)
+        val vh:VH = VH(view)
+        return  vh
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ShiftsBottomListAdapter.VH, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class VH(val view: View) : RecyclerView.ViewHolder(view) {
+        val shiftslistItemRankTextView: TextView
+        val shiftsListItemNameTextView: TextView
+
+        init {
+            shiftslistItemRankTextView = view.findViewById(R.id.action_image);
+            shiftsListItemNameTextView = view.findViewById(R.id.action_image);
+        }
+    }
 }
+
