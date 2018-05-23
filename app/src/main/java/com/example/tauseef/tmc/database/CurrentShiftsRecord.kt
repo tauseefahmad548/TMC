d960670eebfc8191236366d2ca63f6be271be130
Tauseef Ahmad <tauseefahmad548@gmail.com>
2018-05-23 16:50:09 +0500

    init

---
+++ app/src/main/java/com/example/tauseef/tmc/database/CurrentShiftsRecord.kt
@@ -0,0 +1,11 @@
+package com.example.tauseef.tmc.database
+
+import android.arch.persistence.room.Entity
+
+@Entity
+class CurrentShiftsRecord {
+    private val pak: String? = null
+    private val rank: String? = null
+    private val name: String? = null
+    private val shift: ShiftsEnum? = null
+}
