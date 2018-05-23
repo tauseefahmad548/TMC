e3cdf1eaabc0ec7ab449efbe618838b738277244
Tauseef Ahmad <tauseefahmad548@gmail.com>
2018-05-23 16:49:32 +0500

    init

---
+++ app/src/main/java/com/example/tauseef/tmc/database/ShiftsEnum.kt
@@ -0,0 +1,7 @@
+package com.example.tauseef.tmc.database
+
+enum class ShiftsEnum(val shiftName:String?) {
+    MORNING("Morning"),AFTERNOON("Afternoon"),
+    NIGHT("Night"),LEAVE("Leave"),GCREW("GCrew"),
+    SECDUTY("Security Duty"),HOSPITAl("Hospital")
+}
